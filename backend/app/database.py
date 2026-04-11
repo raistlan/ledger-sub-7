ebbfc60de65ca5ae5b72fa7d58fea6ca2517abaf
raistlan <raistlan.schade@gmail.com>
2026-04-11 00:24:53 -0400

    tryign to fix switching db server

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,6 +8,7 @@ engine = create_async_engine(
     pool_pre_ping=True,
     pool_size=10,
     max_overflow=20,
+    connect_args={"server_settings": {"search_path": "public"}},
 )
 
 AsyncSessionLocal = async_sessionmaker(
